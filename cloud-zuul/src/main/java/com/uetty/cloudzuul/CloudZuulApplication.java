ab9135fe159e9c835578a8c11950a81f4a2c0977
dezhang <dezhang@homeinns.com>
2019-04-30 17:04:10 +0800

    geteway 路由功能， cloud的版本升级

--- cloud-zuul/src/main/java/com/uetty/cloudzuul/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/com/uetty/cloudzuul/CloudZuulApplication.java
@@ -2,8 +2,9 @@ package com.uetty.cloudzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+@SpringCloudApplication
 public class CloudZuulApplication {
 
     public static void main(String[] args) {
