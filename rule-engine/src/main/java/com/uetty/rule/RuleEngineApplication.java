d02c3951ae3d335ee7836da1a03812f028406048
dezhang <dezhang@homeinns.com>
2019-05-09 13:41:59 +0800

    新增规则引擎模块

---
+++ rule-engine/src/main/java/com/uetty/rule/RuleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.uetty.rule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class RuleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleEngineApplication.class, args);
+    }
+
+}
