d23fd00dcde95b5d69bb0ea704c8f72c507d8641
koala <vincent_field@foxmail.com>
2019-05-14 23:40:27 +0800

    瞎写，未完成

--- rule-engine/src/main/java/com/uetty/rule/config/redis/template/RedisTemplateRule.java
+++ rule-engine/src/main/java/com/uetty/rule/config/redis/template/RedisTemplateRule.java
@@ -1,20 +1,28 @@
 package com.uetty.rule.config.redis.template;
 
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
-public class RedisTemplateRule extends ReactiveStringRedisTemplate {
+import java.util.Objects;
+
+public class RedisTemplateRule<K,V> extends ReactiveRedisTemplate<K,V> {
 
     public RedisTemplateRule(ReactiveRedisConnectionFactory connectionFactory) {
-        super(connectionFactory, RedisSerializationContext.string());
+        super(connectionFactory, Objects.requireNonNull(redisSerializationContext()));
     }
 
     public RedisTemplateRule(ReactiveRedisConnectionFactory connectionFactory, RedisSerializationContext<String, String> serializationContext) {
-        super(connectionFactory, serializationContext);
+        super(connectionFactory, Objects.requireNonNull(redisSerializationContext()));
     }
 
     public RedisTemplateRule(ReactiveRedisConnectionFactory connectionFactory, RedisSerializationContext<String, String> serializationContext, boolean exposeConnection) {
-        super(connectionFactory, serializationContext, exposeConnection);
+        super(connectionFactory, Objects.requireNonNull(redisSerializationContext()), exposeConnection);
+    }
+
+    private static <K, V> RedisSerializationContext<K, V> redisSerializationContext(){
+        RedisSerializationContext<K,V> redisSerializationContext = (RedisSerializationContext<K, V>) RedisSerializationContext.fromSerializer(RedisSerializer.json());
+        return redisSerializationContext;
     }
 }
