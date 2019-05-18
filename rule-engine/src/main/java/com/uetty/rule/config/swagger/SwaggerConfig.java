8adab4e26d428cd45cd5ec1bfe0be24327f1a88f
koala <vincent_field@foxmail.com>
2019-05-18 20:42:26 +0800

    swagger配置

--- rule-engine/src/main/java/com/uetty/rule/config/swagger/SwaggerConfig.java
+++ rule-engine/src/main/java/com/uetty/rule/config/swagger/SwaggerConfig.java
@@ -2,23 +2,35 @@ package com.uetty.rule.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
-@EnableSwagger2WebFlux
+
 @Configuration
+@EnableSwagger2WebFlux
 public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("规则系统api")
+                .description("简单优雅的restful风格")
+                .version("1.0")
+                .build();
+    }
 
 
     @Bean
     public Docket personApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("all")
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.uetty.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
