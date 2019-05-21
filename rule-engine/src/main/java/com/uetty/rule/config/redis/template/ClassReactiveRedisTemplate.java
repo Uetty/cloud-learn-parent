ef0f2e6240add3bb729c4e847586f94d060b74a3
koala <vincent_field@foxmail.com>
2019-05-21 20:56:37 +0800

    redis 配置

---
+++ rule-engine/src/main/java/com/uetty/rule/config/redis/template/ClassReactiveRedisTemplate.java
@@ -0,0 +1,67 @@
+package com.uetty.rule.config.redis.template;
+
+import com.uetty.rule.config.redis.JacksonRedisSerializer;
+import com.uetty.rule.config.redis.operations.ClassReactiveHashOperations;
+import com.uetty.rule.config.redis.operations.impl.ClassReactiveHashOperationsImpl;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+public class ClassReactiveRedisTemplate<K, V> extends ReactiveRedisTemplate<K, V> {
+
+    public ClassReactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
+        super(connectionFactory, redisSerializationContext());
+    }
+
+    public ClassReactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory, RedisSerializationContext<String, String> serializationContext) {
+        super(connectionFactory, redisSerializationContext());
+    }
+
+    public ClassReactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory, RedisSerializationContext<String, String> serializationContext, boolean exposeConnection) {
+        super(connectionFactory, redisSerializationContext(), exposeConnection);
+    }
+
+    @SuppressWarnings({"unchecked","NullableProblems"})
+    private static <K, V> RedisSerializationContext<K, V> redisSerializationContext() {
+        RedisSerializationContext<String, String> string = RedisSerializationContext.string();
+        RedisSerializationContext<K, V> clazz = (RedisSerializationContext<K, V>) RedisSerializationContext.fromSerializer(new JacksonRedisSerializer<>());
+        return new RedisSerializationContext<K, V>() {
+
+            @Override
+            public SerializationPair getKeySerializationPair() {
+                return string.getKeySerializationPair();
+            }
+
+            @Override
+            public SerializationPair getValueSerializationPair() {
+                return clazz.getValueSerializationPair();
+            }
+
+            @Override
+            public SerializationPair<String> getStringSerializationPair() {
+                return clazz.getStringSerializationPair();
+            }
+
+            @Override
+            public SerializationPair<V> getHashValueSerializationPair() {
+                return clazz.getHashValueSerializationPair();
+            }
+
+            @Override
+            public SerializationPair<K> getHashKeySerializationPair() {
+                return string.getHashKeySerializationPair();
+            }
+        };
+    }
+
+    @Override
+    public <HK, HV> ClassReactiveHashOperations<K, HK, HV> opsForHash() {
+        return opsForHash(redisSerializationContext());
+    }
+
+    @Override
+    public <K1, HK, HV> ClassReactiveHashOperations<K1, HK, HV> opsForHash(RedisSerializationContext<K1, ?> serializationContext) {
+        return new ClassReactiveHashOperationsImpl<>(this, serializationContext);
+    }
+}
+
--- rule-engine/src/main/java/com/uetty/rule/config/redis/template/RedisTemplateRule.java
+++ rule-engine/src/main/java/com/uetty/rule/config/redis/template/RedisTemplateRule.java
@@ -1,66 +1,19 @@
 package com.uetty.rule.config.redis.template;
 
-import com.uetty.rule.config.redis.JacksonRedisSerializer;
-import com.uetty.rule.config.redis.operations.ClassReactiveHashOperations;
-import com.uetty.rule.config.redis.operations.impl.ClassReactiveHashOperationsImpl;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
-public class RedisTemplateRule<K, V> extends ReactiveRedisTemplate<K, V> {
+public class RedisTemplateRule<K, V> extends ClassReactiveRedisTemplate<K, V> {
 
     public RedisTemplateRule(ReactiveRedisConnectionFactory connectionFactory) {
-        super(connectionFactory, redisSerializationContext());
+        super(connectionFactory);
     }
 
     public RedisTemplateRule(ReactiveRedisConnectionFactory connectionFactory, RedisSerializationContext<String, String> serializationContext) {
-        super(connectionFactory, redisSerializationContext());
+        super(connectionFactory, serializationContext);
     }
 
     public RedisTemplateRule(ReactiveRedisConnectionFactory connectionFactory, RedisSerializationContext<String, String> serializationContext, boolean exposeConnection) {
-        super(connectionFactory, redisSerializationContext(), exposeConnection);
-    }
-
-    @SuppressWarnings({"unchecked","NullableProblems"})
-    private static <K, V> RedisSerializationContext<K, V> redisSerializationContext() {
-        RedisSerializationContext<String, String> string = RedisSerializationContext.string();
-        RedisSerializationContext<K, V> clazz = (RedisSerializationContext<K, V>) RedisSerializationContext.fromSerializer(new JacksonRedisSerializer<>());
-        return new RedisSerializationContext<K, V>() {
-
-            @Override
-            public SerializationPair getKeySerializationPair() {
-                return string.getKeySerializationPair();
-            }
-
-            @Override
-            public SerializationPair getValueSerializationPair() {
-                return clazz.getValueSerializationPair();
-            }
-
-            @Override
-            public SerializationPair<String> getStringSerializationPair() {
-                return clazz.getStringSerializationPair();
-            }
-
-            @Override
-            public SerializationPair<V> getHashValueSerializationPair() {
-                return clazz.getHashValueSerializationPair();
-            }
-
-            @Override
-            public SerializationPair<K> getHashKeySerializationPair() {
-                return string.getHashKeySerializationPair();
-            }
-        };
-    }
-
-    @Override
-    public <HK, HV> ClassReactiveHashOperations<K, HK, HV> opsForHash() {
-        return opsForHash(redisSerializationContext());
-    }
-
-    @Override
-    public <K1, HK, HV> ClassReactiveHashOperations<K1, HK, HV> opsForHash(RedisSerializationContext<K1, ?> serializationContext) {
-        return new ClassReactiveHashOperationsImpl<>(this, serializationContext);
+        super(connectionFactory, serializationContext, exposeConnection);
     }
 }
