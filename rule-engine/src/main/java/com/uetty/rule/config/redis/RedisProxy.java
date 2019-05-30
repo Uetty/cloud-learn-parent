3a607467bd38e095d1ae229231f8ab1120088f14
dezhang <dezhang@homeinns.com>
2019-05-30 16:40:52 +0800

    失败了。。等大佬拯救

--- rule-engine/src/main/java/com/uetty/rule/config/redis/RedisProxy.java
+++ rule-engine/src/main/java/com/uetty/rule/config/redis/RedisProxy.java
@@ -1,37 +1,41 @@
 package com.uetty.rule.config.redis;
 
 import com.uetty.rule.entity.User;
-import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Proxy;
 
 /**
  * redis 代理初始化
  */
-@Component
-public class RedisProxyInit<T> {
+public class RedisProxy<T> {
 
-    @SuppressWarnings("unchecked")
-    public void initRedisProxy(T t) {
+    private T t;
+
+    public static <T> T create(T t) {
+        RedisProxy<T> redisProxy = new RedisProxy<>();
+        redisProxy.t = t;
+        redisProxy.initRedisProxy();
+        return redisProxy.t;
+    }
+
+    public void initRedisProxy() {
         Proxy.newProxyInstance(t.getClass().getClassLoader(), t.getClass().getInterfaces(), ((proxy, method, args) -> {
             //GET 方法
             if (method.getName().startsWith("get")) {
                 //判断这个属性是否为空
-                if (method.invoke(proxy, args) == null) {
+                if (method.invoke(t, args) == null) {
                     //设置属性值
                     proxy.getClass().getMethod(method.getName().replace("get", "set"), Object.class)
-                            .invoke(proxy, method.invoke(proxy, args));
+                            .invoke(t, "1");//method.invoke(t, args)
                 }
             }
-            return method.invoke(proxy, args);
+            return method.invoke(t, args);
         }));
-
     }
 
     public static void main(String[] args) {
-        RedisProxyInit<User> userRedisProxyInit = new RedisProxyInit<>();
         User user = new User();
-        userRedisProxyInit.initRedisProxy(user);
+        user = RedisProxy.create(user);
         String userName = user.getUserName();
         System.out.println(userName);
     }
