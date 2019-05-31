d295fef054e294f5b3368f590ccbcf90662cee55
dezhang <dezhang@homeinns.com>
2019-05-31 17:55:23 +0800

    仿redisson分布式锁

--- cloud-feign-api/src/main/java/com/uetty/cloud/feign/api/api/engine/RedisLuaApi.java
+++ cloud-feign-api/src/main/java/com/uetty/cloud/feign/api/api/engine/RedisLuaApi.java
@@ -1,7 +1,7 @@
 package com.uetty.cloud.feign.api.api.engine;
 
 import com.uetty.cloud.feign.api.fallback.HystrixClient;
-import com.uetty.cloud.feign.api.fallback.HystrixClientFallbackFactory;
+import com.uetty.cloud.feign.api.fallback.RedisLuaHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,7 @@ import reactor.core.publisher.Mono;
 /**
  * Redis Lua  脚本 API
  */
-@FeignClient(value = "rule-engine",fallbackFactory = HystrixClientFallbackFactory.class)
+@FeignClient(value = "rule-engine",fallback = RedisLuaHystrix.class)
 @RequestMapping("/redis")
 public interface RedisLuaApi extends HystrixClient {
 
---
+++ cloud-feign-api/src/main/java/com/uetty/cloud/feign/api/fallback/RedisLuaHystrix.java
@@ -0,0 +1,24 @@
+package com.uetty.cloud.feign.api.fallback;
+
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import com.uetty.cloud.feign.api.api.engine.RedisLuaApi;
+import reactor.core.publisher.Mono;
+
+@Hystrix
+public class RedisLuaHystrix implements RedisLuaApi {
+
+    @Override
+    public Mono classPut(Integer userId, String userName) {
+        return null;
+    }
+
+    @Override
+    public Mono classGet(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public Mono getHashFromZset() {
+        return null;
+    }
+}
