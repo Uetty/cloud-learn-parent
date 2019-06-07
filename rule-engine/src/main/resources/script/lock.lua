b8bb39add440c2607b247c66088b44098b69c037
koala <vincent_field@foxmail.com>
2019-06-07 20:04:39 +0800

    redis锁Lua脚本添加注释

--- rule-engine/src/main/resources/script/lock.lua
+++ rule-engine/src/main/resources/script/lock.lua
@@ -1,13 +1,18 @@
-if(redis.call('exists', KEYS[1]) == 0)
+--判断锁key是否存在（key：锁名，hashkey：获取锁的线程）
+if (redis.call('exists', KEYS[1]) == 0)
 then
+    --如果key不存在，则设置值并设置过期时间（代表没有线程占用锁）
     redis.call('hset', KEYS[1], ARGV[2], 1);
     redis.call('pexpire', KEYS[1], ARGV[1]);
     return nil;
-end;
+end ;
+--如果key存在，并且hashkey存在（有线程占用锁）
 if (redis.call('hexists', KEYS[1], ARGV[2]) == 1)
 then
+    --对hash值自增1，并且给key设置过期时间（当前线程在使用锁）
     redis.call('hincrby', KEYS[1], ARGV[2], 1);
     redis.call('pexpire', KEYS[1], ARGV[1]);
     return nil;
-end;
+end ;
+-- key存在，hashkey不存在，返回key的过期时间（其他线程在占用锁，直接返回锁的过期时间）
 return redis.call('pttl', KEYS[1]);
--- rule-engine/src/main/resources/script/unLock.lua
+++ rule-engine/src/main/resources/script/unLock.lua
@@ -1,18 +1,24 @@
+-- 判断key是否存在
 if (redis.call('exists', KEYS[1]) == 0)
 then
+    --如果锁不存在则发布没有锁的消息给其他redis
     redis.call('publish', KEYS[2], ARGV[1]);
     return 1;
 end ;
+-- key存在，判断当前线程是否占有锁
 if (redis.call('hexists', KEYS[1], ARGV[3]) == 0)
 then
     return nil;
 end ;
+-- 当前线程占用锁
 local counter = redis.call('hincrby', KEYS[1], ARGV[3], -1);
 if (counter > 0)
 then
+    -- 没到过期次数，设置过期时间
     redis.call('pexpire', KEYS[1], ARGV[2]);
     return 0;
 else
+    -- 到达过期次数，删除锁key，并发布释放锁
     redis.call('del', KEYS[1]);
     redis.call('publish', KEYS[2], ARGV[1]);
     return 1;
