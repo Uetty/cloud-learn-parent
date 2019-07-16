259d04bcf489fae1ada8bcc70ebd5f8e63171fb6
dezhang <dezhang@homeinns.com>
2019-07-16 09:34:44 +0800

    发票

--- invoice/src/main/java/com/uetty/invoice/InvoiceApplication.java
+++ invoice/src/main/java/com/uetty/invoice/InvoiceApplication.java
@@ -1,13 +1,10 @@
 package com.uetty.invoice;
 
-import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfWriter;
 import com.lowagie.text.pdf.BaseFont;
 import org.xhtmlrenderer.pdf.ITextFontResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -15,17 +12,16 @@ public class InvoiceApplication {
 
     public static void main(String[] args) throws IOException, DocumentException {
         Config config = readConfig();
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream(config.getPath() + "createSamplePDF.pdf"));
-        document.open();
-        document.close();
     }
+
+
 
     /**
      * 生成 PDF 文件
-     * @param out 输出流
+     *
+     * @param out  输出流
      * @param html HTML字符串
-     * @throws IOException IO异常
+     * @throws IOException       IO异常
      * @throws DocumentException Document异常
      */
     public static void createPDF(OutputStream out, String html) throws IOException, com.lowagie.text.DocumentException {
