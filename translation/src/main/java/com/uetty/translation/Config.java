548a4840cf4f95c9e99ed6bb6a6c56440facb5df
dezhang <dezhang@homeinns.com>
2019-08-15 13:28:12 +0800

    发票

--- translation/src/main/java/com/uetty/translation/Config.java
+++ translation/src/main/java/com/uetty/translation/Config.java
@@ -3,6 +3,7 @@ package com.uetty.translation;
 import lombok.Data;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Data
 public class Config {
@@ -14,5 +15,9 @@ public class Config {
     private String xmlPath;
 
     private String translationPath;
+
+    public static void main(String[] args) {
+        System.out.println(Stream.ofNullable(null).count());
+    }
 
 }
